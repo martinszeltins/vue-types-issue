55cc9a05974cc677486f93b8848b392e0930700a
Martins Zeltins <martins.zeltins@arkbauer.com>
2025-02-21 09:08:15 +0200

    Initial commit

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div>
+        <!-- name is a string, so toUpperCase() should work, right? -->
+        {{ name.toUpperCase() }}
+    </div>
 </template>
+
+<script setup lang="ts">
+    const name = ref<string>() // Type: globalThis.Ref<string, string>
+</script>
